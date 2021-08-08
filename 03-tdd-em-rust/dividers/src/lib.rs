3953b41976cc2e5e8e3e99b9adb1a49cde6c746b
melkibalbino <melkibalbinoads@gmail.com>
2021-08-07 21:07:31 -0300

    created test one_is_not_valid and two_divides_two

--- 03-tdd-em-rust/dividers/src/lib.rs
+++ 03-tdd-em-rust/dividers/src/lib.rs
@@ -1,6 +1,10 @@
 #[allow(dead_code)]
-fn divider(num: i64) {
-    panic!("{} <= 0 não é válido", num)
+fn divider(num: i64) -> Vec<i64> {
+    if num <= 1 {
+        panic!("{} <= 1 não é válido", num);
+    } else {
+        vec![2]
+    }
 }
 
 
@@ -11,12 +15,23 @@ mod tests {
     #[test]
     #[should_panic]
     fn zero_is_not_valid() {
-        divider(0)
+        divider(0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn one_is_not_valid() {
+        divider(1);
     }
 
     #[test]
     #[should_panic]
     fn negatives_are_not_valid() {
-        divider(-10)
+        divider(-10);
+    }
+
+    #[test]
+    fn two_divides_two() {
+        assert_eq!(divider(2), vec![2])
     }
 }
